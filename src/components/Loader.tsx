f3571fa9ce474849fe9331598b8068a3c37e70ed
Igor Stepanov <stipyk1309@gmail.com>
2024-03-05 09:41:49 +0300

    fix(layout): loader position fix

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,14 +3,7 @@ import { CircularProgress, Grid } from '@mui/material'
 
 const Loader: React.FC = () => {
 	return (
-		<Grid
-			container
-			sx={{
-				position: 'fixed',
-				top: '30%',
-				left: '50%',
-			}}
-		>
+		<Grid container mt='200px' display='flex' justifyContent='center'>
 			<CircularProgress size={100} color='inherit' />
 		</Grid>
 	)
